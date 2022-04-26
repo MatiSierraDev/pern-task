13e85545d2c526cdc2f35e627d3e004b28e49d3a
matiraw <matisierra.develop@gmail.com>
2022-04-26 00:37:52 -0300

    restart v7

--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ const colors = require('colors')
 const taskRoutes = require('./routes/task.routes')
 const errorNotFound = require('./middleware/error_not_found')
 const handligErrors = require('./middleware/handlig-errors')
-const path  = require('path')
 
 const app = express()
 const port = process.env.PORT || 5000
-
-app.use(express.static(pathth.join(__dirname, 'dist')));
 
 app.use(cors())
 
@@ -18,12 +15,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(morgan('dev'))
-
 app.use(taskRoutes)
 
-app.get('/*', function(req,res) {
-		res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 //middleware 404 not found route
 app.use(errorNotFound)
 

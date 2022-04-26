3dafa74371d5a6fdf5a2a23b6dcc1ab8b11f2648
matiraw <matisierra.develop@gmail.com>
2022-04-25 22:54:17 -0300

    static deploy

--- src/index.js
+++ src/index.js
@@ -19,10 +19,6 @@ app.use(taskRoutes)
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 //middleware 404 not found route
 app.use(errorNotFound)
 
